866273ae50a291e4f321d55a7508d71934bb2e83
hazimhamid <hazimhamid5@gmail.com>
2024-03-13 23:04:03 +0500

    favoriteSchema

---
+++ models/favoriteModel.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const favoriteSchema = new Schema({
+  user_id: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  product_id: {
+    type: Schema.Types.ObjectId,
+    ref: "Product",
+  },
+  created_at: { type: Date, default: Date.now },
+});
+
+const Favorite = mongoose.model("Favorite", favoriteSchema);
+
+module.exports = Favorite;
