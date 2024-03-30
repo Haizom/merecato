3dc8130fc302fb53a3e4b01c1b344c4467857cb9
hazimhamid <hazimhamid5@gmail.com>
2024-03-31 04:23:18 +0500

    user image

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,19 +1,24 @@
-const express = require('express')
-const router = express.Router()
-
+const express = require("express");
+const router = express.Router();
+const upload = require("../config/storageConfig");
 
 const {
-    registerUser,
-    login,
-    logout,
-    updateUserInfo
-} = require('../controllers/userController')
+  registerUser,
+  login,
+  logout,
+  updateUserInfo,
+} = require("../controllers/userController");
 
-const { userAuth } = require('../middleware/userAuth')
+const { userAuth } = require("../middleware/userAuth");
 
-router.post('/login', login)
-router.post('/register', registerUser)
-router.post('/logout', userAuth, logout)
-router.patch('/update-user-info', userAuth, updateUserInfo)
+router.post("/login", login);
+router.post("/register", registerUser);
+router.post("/logout", userAuth, logout);
+router.patch(
+  "/update-user-info",
+  userAuth,
+  upload.single("image"),
+  updateUserInfo
+);
 
-module.exports = router
+module.exports = router;
