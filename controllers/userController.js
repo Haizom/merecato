5c54791a11e6f106b8c747bfb3c8f66f19f9eceb
hazimhamid <hazimhamid5@gmail.com>
2024-05-05 15:32:12 +0300

    user edits -2

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,105 +1,105 @@
-const asyncHandler = require('express-async-handler');
-const User = require('../models/UserModel');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-
+const asyncHandler = require("express-async-handler");
+const User = require("../models/UserModel");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 // Login
 const login = asyncHandler(async (req, res) => {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
 
-    if (!user) {
-        return res.status(401).json({ message: 'User does not exist' });
-    }
+  if (!user) {
+    return res.status(401).json({ message: "User does not exist" });
+  }
 
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-        return res.status(401).json({ message: 'Invalid password' });
-    }
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    return res.status(401).json({ message: "Invalid password" });
+  }
 
-    const payload = { id: user.id };
-    const options = { expiresIn: '1y' };
-    const token = jwt.sign(payload, process.env.JWT_SECRET, options);
+  const payload = { id: user.id };
+  const options = { expiresIn: "1y" };
+  const token = jwt.sign(payload, process.env.JWT_SECRET, options);
 
-    res.status(200).json({ token, user: user});
+  res.status(200).json({ token, user: user });
 });
-
 
 // Register
 const registerUser = asyncHandler(async (req, res) => {
-    const { firstName, lastName, email, phone, location, birthday, password } = req.body;
+  const { firstName, lastName, email, phone, location, birthday, password } =
+    req.body;
 
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-        return res.status(400).json({ message: 'Email is already registered' });
-    }
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    return res.status(400).json({ message: "Email is already registered" });
+  }
 
-    const hashedPassword = await bcrypt.hash(password, 10);
+  const hashedPassword = await bcrypt.hash(password, 10);
 
-    const newUser = new User({
-        firstName,
-        lastName,
-        email,
-        phone,
-        location,
-        birthday,
-        password: hashedPassword,
-    });
+  const newUser = new User({
+    firstName,
+    lastName,
+    email,
+    phone,
+    location,
+    birthday,
+    password: hashedPassword,
+  });
 
-    await newUser.save();
-    res.status(201).json({ message: 'User registered successfully' });
+  await newUser.save();
+  res.status(201).json({ message: "User registered successfully" });
 });
-
 
 // Logout -- just for checking the auth middleware
 const logout = asyncHandler(async (req, res) => {
+  // blacklist ?
 
-    // blacklist ?
-
-    res.status(200).json({ message: 'Logout successful' });
+  res.status(200).json({ message: "Logout successful" });
 });
 
 // Updating user and adding image
 const updateUserInfo = asyncHandler(async (req, res) => {
-    const { firstName, lastName, phone, location, birthday } = req.body;
-    // console.log(req.body)
+  const { firstName, lastName, phone, location, birthday, description } =
+    req.body;
+  // console.log(req.body)
 
-    const profileImage = req.file ? req.file.filename : null; 
+  const profileImage = req.file ? req.file.filename : null;
 
-    const userId = req.user.id; 
-    // console.log(userId)
+  const userId = req.user.id;
+  // console.log(userId)
 
-    try {
-        const user = await User.findById(userId);
+  try {
+    const user = await User.findById(userId);
 
-        if (!user) {
-            return res.status(404).json({ message: 'User not found' });
-        }
-
-        user.firstName = firstName || user.firstName;
-        user.lastName = lastName || user.lastName;
-        user.phone = phone || user.phone;
-        user.location = location || user.location;
-        user.birthday = birthday || user.birthday;
-
-        if (profileImage) {
-            user.image = profileImage;
-        }
-
-        await user.save();
-
-        res.status(200).json({ message: 'User information updated successfully', user });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Internal Server Error' });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
+
+    user.firstName = firstName || user.firstName;
+    user.lastName = lastName || user.lastName;
+    user.phone = phone || user.phone;
+    user.location = location || user.location;
+    user.birthday = birthday || user.birthday;
+    user.description = description || user.description;
+
+    if (profileImage) {
+      user.image = profileImage;
+    }
+
+    await user.save();
+
+    res
+      .status(200)
+      .json({ message: "User information updated successfully", user });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
-
 module.exports = {
-    registerUser,
-    login,
-    logout,
-    updateUserInfo
+  registerUser,
+  login,
+  logout,
+  updateUserInfo,
 };
--- models/UserModel.js
+++ models/UserModel.js
@@ -1,43 +1,50 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-        firstName: {
-            type: String,
-            required: true,
-        },
-        lastName: {
-            type: String,
-            required: true,
-        },
-        email: {
-            type: String,
-            unique: true,
-            required: true,
-        },
-        phone: {
-            type: String,
-            required: true,
-        },
-        location: {
-            type: String,
-            required: true,
-        },
-        birthday: {
-            type: Date,
-            required: true,
-        },
-        password: {
-            type: String,
-            required: true,
-        },
-        image: {
-            type: String,
-            required: false,
-        },
-        }, {
-        timestamps: true,
-    });
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: false,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    birthday: {
+      type: Date,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
