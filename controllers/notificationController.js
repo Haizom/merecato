7a6f1182b4d6ff3590e07af0a6bfb0eebfee1e5e
hazimhamid <hazimhamid5@gmail.com>
2024-05-21 22:34:54 +0300

    notifivations crud and realtime

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -1,9 +1,10 @@
 const asyncHandler = require("express-async-handler");
 const mongoose = require("mongoose");
 const Notification = require("../models/notificationModel");
-const io = require("../socket/socket"); 
+const { getReceiverSocketId, io } = require("../socket/socket");
 
 // Create a notification
+// needs rewritting
 const createNotification = asyncHandler(async (req, res) => {
   const { content } = req.body;
   console.log(content)
@@ -15,11 +16,68 @@ const createNotification = asyncHandler(async (req, res) => {
 
   const notification = new Notification({ userId, content });
   await notification.save();
-  io.getIO().emit("notification", { action: "create", notification });
+    // adding real time messages with socket
+  const receiverSocketId = getReceiverSocketId(userId);
+  if (receiverSocketId) {
+    // io.to(<socket_id>).emit() used to send events to specific client
+    io.to(receiverSocketId).emit("notification", notification);
+  }
   return notification
 });
 
 
+// Fetch notifications for a specific user
+const getNotifications = asyncHandler(async (req, res) => {
+  const userId = req.user.id;
+
+  if (!mongoose.Types.ObjectId.isValid(userId)) {
+    return res.status(400).json({ error: "Invalid user ID!" });
+  }
+
+  const notifications = await Notification.find({ userId });
+  res.status(200).json(notifications);
+});
+
+// Mark a notification as read
+const markNotificationAsRead = asyncHandler(async (req, res) => {
+  const { notificationId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(notificationId)) {
+    return res.status(400).json({ error: "Invalid notification ID!" });
+  }
+
+  const notification = await Notification.findById(notificationId);
+  if (!notification) {
+    return res.status(404).json({ error: "Notification not found!" });
+  }
+
+  notification.isRead = true;
+  await notification.save();
+
+  res.status(200).json(notification);
+});
+
+// Delete a notification
+const deleteNotification = asyncHandler(async (req, res) => {
+  const { notificationId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(notificationId)) {
+    return res.status(400).json({ error: "Invalid notification ID!" });
+  }
+
+  const notification = await Notification.findById(notificationId);
+  if (!notification) {
+    return res.status(404).json({ error: "Notification not found!" });
+  }
+
+  await notification.remove();
+
+  res.status(204).end();
+});
+
 module.exports = {
   createNotification,
+  deleteNotification,
+  markNotificationAsRead,
+  getNotifications
 };
--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -1,9 +1,17 @@
 const express = require("express");
-const { createNotification } = require("../controllers/notificationController");
+const { 
+  createNotification,
+  getNotifications,
+  markNotificationAsRead,
+  deleteNotification
+} = require("../controllers/notificationController");
 const { userAuth } = require("../middleware/userAuth");
 
 const notificationRoutes = express.Router();
 
 notificationRoutes.post("/", userAuth, createNotification);
+notificationRoutes.get("/", userAuth, getNotifications);
+notificationRoutes.put("/:notificationId/mark-as-read", userAuth, markNotificationAsRead);
+notificationRoutes.delete("/:notificationId", userAuth, deleteNotification);
 
 module.exports = notificationRoutes;
